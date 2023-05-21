fdd1d60cb068257f8135112f07c8f4949c4c86fa
David Morales <morales.david1997@gmail.com>
2023-05-21 12:58:57 -0400

    feat(constants): :sparkles: add operation constants

---
+++ src/constants/operations.constants.ts
@@ -0,0 +1,34 @@
+import { OperationWidgetMapping } from "../types";
+
+export const operationWidgetMapping: OperationWidgetMapping = {
+  addition: {
+    symbol: "+",
+    btnColor: "primary",
+    size: "md-3",
+  },
+  subtraction: {
+    symbol: "-",
+    btnColor: "primary",
+    size: "md-3",
+  },
+  multiplication: {
+    symbol: "*",
+    btnColor: "primary",
+    size: "md-3",
+  },
+  division: {
+    symbol: "/",
+    btnColor: "primary",
+    size: "md-3",
+  },
+  squareRoot: {
+    symbol: "√",
+    btnColor: "primary",
+    size: "md-3",
+  },
+  random_string: {
+    symbol: "Random String",
+    btnColor: "secondary",
+    size: "md-12",
+  },
+};
