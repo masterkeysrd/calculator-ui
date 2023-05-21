ac28232bf4bef544b1a28258be15f6919d9d1048
David Morales <morales.david1997@gmail.com>
2023-05-21 19:23:24 -0400

    feat(common/auth): :sparkles: add useIsAuthenticated and useLogin function

---
+++ src/common/auth/login.ts
@@ -0,0 +1,55 @@
+import { useJwt } from "@vueuse/integrations/useJwt";
+import { useAccessToken } from "./token";
+import { ref, watch } from "vue";
+import AuthService from "../../services/AuthService";
+import { useRouter } from "vue-router";
+import { AxiosError } from "axios";
+
+export function useIsAuthenticated() {
+  const token = useAccessToken();
+
+  const authenticated = ref(false);
+  const { payload } = useJwt(token);
+
+  function checkToken() {
+    if (!payload.value) {
+      authenticated.value = false;
+      return;
+    }
+
+    const { exp } = payload.value as any;
+    const now = Date.now() / 1000;
+    console.log(exp, now);
+
+    if (exp > now) {
+      authenticated.value = true;
+      return;
+    }
+
+    authenticated.value = false;
+  }
+
+  watch(payload, checkToken, { immediate: true });
+
+  return authenticated;
+}
+
+export function useLogin() {
+  const token = useAccessToken();
+  const router = useRouter();
+  const error = ref("");
+
+  async function login(username: string, password: string) {
+    try {
+      const tokens = await AuthService.login(username, password);
+      token.value = tokens.access_token;
+      router.push("/");
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        error.value = err.response?.data?.message;
+      }
+    }
+  }
+
+  return {login, error  };
+} 
