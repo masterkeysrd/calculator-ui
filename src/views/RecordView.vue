31caf4cfd51a15de24ee9454e88e4ba2ad32a89d
David Morales <morales.david1997@gmail.com>
2023-05-21 12:08:38 -0400

    feat(views/record): :sparkles: add record view

---
+++ src/views/RecordView.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-sheet rounded="lg" class="pa-5">
+      <h1>Records</h1>
+      <RecordList :records="records" />
+    </v-sheet>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import RecordList from "../components/record/RecordList.vue";
+import { useListRecords } from '../services/record.service';
+
+export default defineComponent({
+  name: "RecordView",
+  components: {
+    RecordList,
+  },
+});
+</script>
+
+<script lang="ts" setup>
+  const { records } = useListRecords();
+</script>
