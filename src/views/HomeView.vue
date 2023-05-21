a26ecb69fd87d40e7fa5be59f8784b56b6884c55
David Morales <morales.david1997@gmail.com>
2023-05-21 12:06:00 -0400

    feat(views/home): :sparkles: add home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container style="max-width: 600px">
+    <div>
+      <h2>Balance: $200</h2>
+    </div>
+    <v-sheet rounded>
+      <Calculator />
+    </v-sheet>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Calculator from "../components/calculator/Calculator.vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    Calculator,
+  },
+});
+</script>
