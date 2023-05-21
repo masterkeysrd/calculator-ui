3d7749d7892cf138f46d140e868e343ce02c38ef
David Morales <morales.david1997@gmail.com>
2023-05-21 12:56:35 -0400

    feat(types): :sparkles: add operation types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,21 @@
+export type OperationWidget = Operation & OperationWidgetMeta;
+
+export type Operation = {
+  id: number;
+  type: string;
+  cost: number;
+};
+
+export interface OperationWidgetMapping {
+  [key: string]: OperationWidgetMeta;
+}
+
+export interface OperationWidgetMeta {
+  symbol: string;
+  btnColor: string;
+  size: string;
+}
+
 export interface Record {
   id: number;
   userId: number;
