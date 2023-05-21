26ed95a16192cdde24c012480c6f2bfd41ec811e
David Morales <morales.david1997@gmail.com>
2023-05-21 01:13:13 -0400

    feat(views): :sparkles: add features to login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,43 @@
 <template>
-  <form id="login-form">
-    <div>
-      <label for="username">Username</label>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        v-model="store.username"
-      />
+  <div class="d-flex justify-center align-center h-screen">
+    <div class="login-container">
+      <h1 class="text-center mb-5">Login</h1>
+      <v-form id="login-form">
+        <v-text-field
+          id="username"
+          name="username"
+          v-model="store.username"
+          label="Username"
+        ></v-text-field>
+        <v-text-field
+          id="password"
+          label="Password"
+          name="password"
+          type="password"
+          v-model="store.password"
+        ></v-text-field>
+        <v-btn
+          color="primary"
+          variant="elevated"
+          size="large"
+          class="w-100"
+          @click.prevent="login"
+          >Login</v-btn
+        >
+      </v-form>
+      <div class="mt-5">
+        <v-btn
+          text
+          color="secondary"
+          size="large"
+          class="w-100"
+          @click="$router.push('/register')"
+        >
+          Create new account
+        </v-btn>
+      </div>
     </div>
-    <div>
-      <label for="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        v-model="store.password"
-      />
-    </div>
-    <button type="submit" @click.prevent="login">Login</button>
-  </form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -34,13 +52,13 @@ export default defineComponent({
       store: LoginStore,
     };
   },
+  mounted() {
+    if (this.store.isLoggedIn) {
+      this.$router.push("/");
+    }
+  },
   methods: {
     async login() {
-      console.log("login", {
-        username: this.store.username,
-        password: this.store.password,
-      });
-
       const response = await AuthService.login(
         this.store.username,
         this.store.password
@@ -50,7 +68,14 @@ export default defineComponent({
         response;
 
       this.store.login({ accessToken, refreshToken });
+      this.$router.push("/");
     },
   },
 });
 </script>
+
+<style scoped>
+.login-container {
+  width: 400px;
+}
+</style>
