f96ab29fea0970831c351609d345bde062bb1c5b
David Morales <morales.david1997@gmail.com>
2023-05-21 21:56:46 -0400

    feat(views/main): :sparkles: load profile when view is mounted

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,22 +1,31 @@
 <template>
-    <v-app>
-        <NavBar />
-        <v-main class="bg-grey-lighten-3">
-            <v-container>
-                <router-view />
-            </v-container>
-        </v-main>
-    </v-app>
+  <v-app>
+    <NavBar />
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 import NavBar from "../layout/NavBar.vue";
+import { useLoadProfile } from "../stores/profile.store";
 
 export default defineComponent({
-    name: "HomeView",
-    components: {
-        NavBar,
-    },
+  name: "HomeView",
+  components: {
+    NavBar,
+  },
+});
+</script>
+
+<script lang="ts" setup>
+const loadProfile = useLoadProfile();
+
+onMounted(() => {
+  loadProfile();
 });
 </script>
