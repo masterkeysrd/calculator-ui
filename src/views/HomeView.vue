f764fa6d1cb992917c29fded2d4aa863ac730fa0
David Morales <morales.david1997@gmail.com>
2023-05-22 13:47:24 -0400

    refactor(views/home): :recycle: refactor to use kebab case

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,26 @@
 <template>
   <v-container style="max-width: 600px">
     <div class="mb-4 d-flex justify-end">
-      <div style="max-width: 200px;">
-        <BalanceCard />
+      <div style="max-width: 200px">
+        <calculator-balance-card />
       </div>
     </div>
     <v-sheet rounded>
-      <Calculator />
+      <calculator-screen />
     </v-sheet>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Calculator from "../components/calculator/Calculator.vue";
-import BalanceCard from "../components/calculator/BalanceCard.vue";
+import CalculatorBalanceCard from "../components/calculator/CalculatorBalanceCard.vue";
+import CalculatorScreen from "../components/calculator/CalculatorScreen.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    Calculator,
-    BalanceCard
-},
+    CalculatorBalanceCard,
+    CalculatorScreen,
+  },
 });
 </script>
