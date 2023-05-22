ea2d6fa54afd28167027e4183b51fe6184a79a22
David Morales <morales.david1997@gmail.com>
2023-05-21 21:53:18 -0400

    feat(components/user): :sparkles: add welcome user component

---
+++ src/components/users/WelcomeUser.vue
@@ -0,0 +1,26 @@
+<template>
+  <template v-if="profile">
+    <span class="text-teal-darken-2 welcome-text"
+      >Hi {{ profile.username }}!</span
+    >
+  </template>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useProfile } from "../../stores/profile.store";
+
+defineComponent({
+  name: "WelcomeUser",
+});
+</script>
+
+<script lang="ts" setup>
+const { profile } = useProfile();
+</script>
+
+<style scoped>
+.welcome-text {
+  font-weight: 600;
+}
+</style>
