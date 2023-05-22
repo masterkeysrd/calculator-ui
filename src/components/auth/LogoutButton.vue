589d5e6010b46f84a5bc3b967b263906a2046719
David Morales <morales.david1997@gmail.com>
2023-05-21 21:44:20 -0400

    feat(components/auth): :sparkles: add logout button component

---
+++ src/components/auth/LogoutButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn
+    color="secondary"
+    size="small"
+    variant="elevated"
+    class="w-100"
+    @click="logout"
+  >
+    Logout
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useLogout } from "../../common/auth/login";
+
+export default defineComponent({
+  name: "LogoutButton",
+});
+</script>
+
+<script lang="ts" setup>
+const logout = useLogout();
+</script>
