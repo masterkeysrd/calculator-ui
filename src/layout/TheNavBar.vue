2ceffebf006e7912ea80d140e8c9269dc528a7e2
David Morales <morales.david1997@gmail.com>
2023-05-22 13:43:56 -0400

    refactor(layout): :recycle: update and rename NavBar

---
+++ src/layout/TheNavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn v-for="link in links" :key="link.name" :to="link.path" text>
+    {{ link.name }}
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "the-nav-bar",
+});
+</script>
+
+<script lang="ts" setup>
+const links = [
+  { name: "Home", path: "/home" },
+  { name: "Records", path: "/records" },
+];
+</script>
