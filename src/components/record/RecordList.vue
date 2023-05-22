f689d57469a28f48177710a23f903b3590d15b35
David Morales <morales.david1997@gmail.com>
2023-05-21 22:35:46 -0400

    feat(components/record): :sparkles: add formatting for records

--- src/components/record/RecordList.vue
+++ src/components/record/RecordList.vue
@@ -13,18 +13,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="record in props.records" :key="record.id">
+        <tr v-for="record in records" :key="record.id">
           <td>{{ record.id }}</td>
           <!-- TODO: Operation type should be added -->
           <td>Testing</td>
           <td>{{ record.amount }}</td>
-          <td >{{ record.userBalance }}</td>
-          <td >{{ record.date }}</td>
+          <td>{{ record.userBalance }}</td>
+          <td>{{ record.date }}</td>
           <td c>{{ record.result }}</td>
           <td class="text-end">
-            <v-btn size="small" color="error" text>
-              Delete
-            </v-btn>
+            <v-btn size="small" color="error" text> Delete </v-btn>
           </td>
         </tr>
       </tbody>
@@ -33,7 +31,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
   name: "RecordList",
@@ -41,11 +39,25 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
+import { useFormatCurrency } from "../../common/utils/currency.util";
 import { Record } from "../../types";
 
 interface RecordProps {
   records: Record[];
 }
+
+const formatCurrency = useFormatCurrency();
+
+const records = computed(() => {
+  return props.records.map((record) => {
+    return {
+      ...record,
+      userBalance: formatCurrency(record.userBalance),
+      amount: formatCurrency(record.amount),
+      date: new Date(record.date).toLocaleDateString(),
+    };
+  });
+});
 
 const props = defineProps<RecordProps>();
 </script>
