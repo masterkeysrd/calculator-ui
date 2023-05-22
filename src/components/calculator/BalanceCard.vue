5568c653bcaee5e600ae6cd9dc859c118a816096
David Morales <morales.david1997@gmail.com>
2023-05-21 21:50:47 -0400

    feat(components/calculator): :sparkles: add balance card component

---
+++ src/components/calculator/BalanceCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-row class="text-end">
+    <v-col cols="6" md="6" class="py-0">
+      <span class="text-h6">Balance</span>
+    </v-col>
+    <v-col cols="6" md="6" class="py-0">
+      <span class="text-h6 fw-bold" v-bind:class="balanceClass">
+        {{ balanceText }}
+      </span>
+    </v-col>
+    <v-col cols="6" md="6">
+      <span class="text-h6">In Flight</span>
+    </v-col>
+    <v-col>
+      <span class="text-h6 fw-bold">
+        {{ inFlightBalanceText }}
+      </span>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, onMounted } from "vue";
+import { useFormatCurrency } from "../../common/utils/currency.util";
+import {
+  useProfileBalance,
+  useLoadProfileBalance,
+} from "../../stores/profile.store";
+
+export default defineComponent({
+  name: "BalanceCard",
+});
+</script>
+
+<script lang="ts" setup>
+const { balance } = useProfileBalance();
+const loadProfileBalance = useLoadProfileBalance();
+const formatCurrency = useFormatCurrency();
+
+onMounted(() => {
+  loadProfileBalance();
+});
+
+const balanceClass = computed(() => {
+  return balance.value.amount > 0 ? "text-success" : "text-error";
+});
+
+const balanceText = computed(() => {
+  return formatCurrency(balance.value.amount);
+});
+
+const inFlightBalanceText = computed(() => {
+  return formatCurrency(balance.value.inFlightAmount);
+});
+</script>
