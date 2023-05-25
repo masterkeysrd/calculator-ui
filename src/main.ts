d445f55d504940e5e2f802200c1dde5187abddf0
David Morales <morales.david1997@gmail.com>
2023-05-25 19:24:05 -0400

    fix(root): :bug: correct dependencies problems with vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import router from "./router";
 import App from "./App.vue";
 
 import "vuetify/styles";
+import '@mdi/font/css/materialdesignicons.css'; 
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import * as labsComponents from 'vuetify/labs/components'
 
 const vuetify = createVuetify({
-  components,
+  components: { ...components, ...labsComponents },
   directives,
 });
 
