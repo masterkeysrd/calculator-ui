fde309de3ae1fe3750d9b31e6752d83b96b91113
David Morales <morales.david1997@gmail.com>
2023-05-26 18:26:28 -0400

    feat(components/common): :sparkles: add custom dialogs

---
+++ src/components/common/ConfirmDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-dialog
+    :model-value="$props.modelValue"
+    @update:model-value="$emit('update:modelValue', $event)"
+    persistent
+    width="300"
+  >
+    <v-card class="text-center">
+      <v-card-title class="headline">Delete Record</v-card-title>
+      <v-card-text>Are you sure you want to perform this action?</v-card-text>
+      <v-card-actions class="justify-center">
+        <v-btn color="primary" @click="cancel">Cancel</v-btn>
+        <v-btn color="error" @click="confirm">Accept</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts" setup>
+defineProps(["modelValue"]);
+const emits = defineEmits(["update:modelValue", "confirm", "cancel"]);
+
+const confirm = () => {
+  emits("update:modelValue", false);
+  emits("confirm");
+};
+
+const cancel = () => {
+  emits("update:modelValue", false);
+  emits("cancel");
+};
+</script>
---
+++ src/components/common/LoadingDialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-dialog
+    :model-value="$props.modelValue"
+    width="400"
+    persistent
+  >
+    <v-card class="text-center" height="150">
+      <v-card-text>
+        <div class="mb-5">
+          Loading...
+        </div>
+        <v-progress-circular
+          indeterminate
+          size="64"
+          color="primary"
+        ></v-progress-circular>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts" setup>
+defineProps(["modelValue"]);
+</script>
